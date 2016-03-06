ece506db176a15bf78d0b358745772a2fd34c092
Ryan Bahniuk <ryan@wealthfront.com>
2016-03-06 15:59:51 -0800

    reorganizing structure and adding tests

---
+++ src/build-sign.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const MAX_SIGN_WIDTH = 10;
+const LINE_DELIMITER = '\n';
+
+function buildBunny() {
+  return '(\__/) ||\n(•ㅅ•) ||\n/ 　 づ';
+}
+
+function buildBottomOfSign() {
+  return `|${'＿'.repeat(MAX_SIGN_WIDTH)}|`;
+}
+
+function buildTopOfSign() {
+  return `|${'￣'.repeat(MAX_SIGN_WIDTH)}|`;
+}
+
+function buildMiddleOfSign(inputArr) {
+  const lines = inputArr.map((word) => {
+    if (word.length > (MAX_SIGN_WIDTH - 3)) throw 'One of your words is too long.'
+    return `| ${word}${' '.repeat(MAX_SIGN_WIDTH - 3 - word.length)}|`;
+  });
+  return lines.join(LINE_DELIMITER);
+}
+
+function buildSign(inputArr) {
+    return [
+            buildTopOfSign(),
+            buildMiddleOfSign(inputArr),
+            buildBottomOfSign(),
+            buildBunny()
+           ].join(LINE_DELIMITER);
+}
+
+module.exports = buildSign;
--- src/sign-bunny.js
+++ src/sign-bunny.js
@@ -1,7 +1,11 @@
 'use strict';
 
-function signBunny(input) {
+const buildSign = require('./build-sign');
 
+function signBunny(input) {
+  const inputArr = input.toUpperCase().split(' ');
+
+  return buildSign(inputArr);
 }
 
 module.exports = signBunny;
