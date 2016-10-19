e03379541a305c676ef088495a213f8359d121e0
tao <tao@tinygaragelabs.com>
2016-10-18 21:52:12 -0700

    failing test

--- test/build-sign-test.js
+++ test/build-sign-test.js
@@ -30,6 +30,32 @@ describe('#buildSign', () => {
     });
   });
 
+  context('when there are short words that can fit on the same line', () => {
+    beforeEach(() => {
+      inputArr = [
+        'I',
+        'AM',
+        'A',
+        'CUTE',
+        'BUNNY'
+      ];
+    });
+
+    it('should return the complete sign string', () => {
+      assert.strictEqual(
+        buildSign(inputArr),
+        '|-----------|\n' +
+        '| I AM A    |\n' +
+        '| CUTE      |\n' +
+        '| BUNNY     |\n' +
+        '|-----------|\n' +
+        '(\\__/) ||\n' +
+        '(•ㅅ•) ||\n' +
+        '/ 　 づ'
+      );
+    });
+  });
+
   context('when one of the words is longer than the max width', () => {
     beforeEach(() => {
       inputArr = [
